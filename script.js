5a65cce2a9cf1227f83a11e37084366aca15a5c5
Skoco Pavle <skocopavle@gmail.com>
2025-06-28 19:49:23 +0200

    Variable in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const prompt = require("prompt-sync")()
-const v = prompt("What is your name ")
-console.log(`Hello ${v}`);
+// const prompt = require("prompt-sync")()
+
+var helloWorld = "hello"
+helloWorld = "world"
+console.log(helloWorld)
+// This type of variables can be changerd over the time
+
+let x = 5
+x += 4
+console.log(x)
+// This type of the variable is similar to the var, this one can also be changed later on
+
+const VELOCITY = 34
+console.log(VELOCITY)
+// This type of the variable is constant and can be changed over the time, even if we want to
