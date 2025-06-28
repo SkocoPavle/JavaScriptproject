0bea6094a3b71a6efd13609f04f139cadcb7eb7e
Skoco Pavle <skocopavle@gmail.com>
2025-06-28 20:07:49 +0200

    Arithmetic operators and type coercion

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-// const prompt = require("prompt-sync")()
+const x = "7"
+const y = 6
 
-var helloWorld = "hello"
-helloWorld = "world"
-console.log(helloWorld)
-// This type of variables can be changerd over the time
-
-let x = 5
-x += 4
-console.log(x)
-// This type of the variable is similar to the var, this one can also be changed later on
-
-const VELOCITY = 34
-console.log(VELOCITY)
-// This type of the variable is constant and can be changed over the time, even if we want to
+console.log(x + y)
+console.log(x * y)
