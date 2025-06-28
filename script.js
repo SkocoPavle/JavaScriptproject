03d03ee7e1a5c1123a252f1bcf373bd40454ecfd
Skoco Pavle <skocopavle@gmail.com>
2025-06-28 20:18:48 +0200

    Type conversions

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const x = "7"
+const x = "2345.8"
+const p = "235px"
+const k = "235.5asas"
+const c = 25
 const y = 6
 
-console.log(x + y)
-console.log(x * y)
+console.log(Number(x) + y)
+console.log(Number(true) + y)
+console.log(Number(false) + y)
+// Alows us to convert string into a number
+
+console.log(parseInt(p) + y)
+// Alows us to convert number inside a string to an acutal number
+
+console.log(parseFloat(k) + y)
+// Alows us to convert float number inside a string to an actual number
+
+console.log(String(c) + y)
+console.log(c + "")
+console.log(c.toString())
+// Convers number into a string
